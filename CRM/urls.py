a2e67f903c05e145e655b7849a5ea8347256e868
abhishek-ag2000 <abhishek.ag2000@gmail.com>
2019-08-27 11:54:35 +0530

    changes

---
+++ CRM/urls.py
@@ -0,0 +1,12 @@
+"""
+URLs
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+#from . import 
+
+urlpatterns = [
+    url(r'^CRMdashboard/'$,  ,name="CRMdashboard"),
+
+]
--- CRM/views.py
+++ CRM/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+import os
+import json
+import requests
+import datetime
 # Create your views here.
