c487791d0d8ba28b78aa91ccb7b67f3b649f282c
uttam166 <166uttam@gmail.com>
2019-08-27 14:09:50 +0530

    update models_accounts, models_contacts

--- CRM/models_accounts.py
+++ CRM/models_accounts.py
@@ -3,11 +3,13 @@ from django.db import models
 from django.utils.translation import pgettext_lazy
 from django.utils.translation import ugettext_lazy as _
 
-from common.models import User
-from common.utils import INDCHOICES, COUNTRIES
+# from common.models import User
+from django.conf import settings    #import user
+from company.models import Company  #import company
+from CRMcommon.utils import INDCHOICES, COUNTRIES
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.text import slugify
-from . import Contact
+from .models_contacts import Contact
 
 
 class Tags(models.Model):
@@ -25,6 +27,18 @@ class Account(models.Model):
         ("open", "Open"),
         ('close', 'Close')
     )
+    # adding company details
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_accounts')
+
+    # adding user
+    created_by = models.ForeignKey(
+            settings.AUTH_USER_MODEL, related_name='account_created_by',
+            on_delete=models.SET_NULL, null=True)
+    assigned_to = models.ManyToManyField(
+            settings.AUTH_USER_MODEL, related_name='account_assigned_users')
+
+
+
 
     name = models.CharField(pgettext_lazy(
         "Name of Account", "Name"), max_length=64)
@@ -52,9 +66,7 @@ class Account(models.Model):
         max_length=3, choices=COUNTRIES, blank=True, null=True)
     website = models.URLField(_("Website"), blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    created_by = models.ForeignKey(
-        User, related_name='account_created_by',
-        on_delete=models.SET_NULL, null=True)
+    
     created_on = models.DateTimeField(_("Created on"), auto_now_add=True)
     is_active = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tags, blank=True)
@@ -67,8 +79,7 @@ class Account(models.Model):
         "Name of Contact", "Contact Name"), max_length=120)
     contacts = models.ManyToManyField(
         'contacts.Contact', related_name="account_contacts")
-    assigned_to = models.ManyToManyField(
-        User, related_name='account_assigned_users')
+    
 
     def __str__(self):
         return self.name
@@ -76,45 +87,45 @@ class Account(models.Model):
     class Meta:
         ordering = ['-created_on']
 
-    def get_complete_address(self):
-        address = ""
-        if self.billing_address_line:
-            address += self.billing_address_line
-        if self.billing_street:
-            if address:
-                address += ", " + self.billing_street
-            else:
-                address += self.billing_street
-        if self.billing_city:
-            if address:
-                address += ", " + self.billing_city
-            else:
-                address += self.billing_city
-        if self.billing_state:
-            if address:
-                address += ", " + self.billing_state
-            else:
-                address += self.billing_state
-        if self.billing_postcode:
-            if address:
-                address += ", " + self.billing_postcode
-            else:
-                address += self.billing_postcode
-        if self.billing_country:
-            if address:
-                address += ", " + self.get_billing_country_display()
-            else:
-                address += self.get_billing_country_display()
-        return address
+    # def get_complete_address(self):
+    #     address = ""
+    #     if self.billing_address_line:
+    #         address += self.billing_address_line
+    #     if self.billing_street:
+    #         if address:
+    #             address += ", " + self.billing_street
+    #         else:
+    #             address += self.billing_street
+    #     if self.billing_city:
+    #         if address:
+    #             address += ", " + self.billing_city
+    #         else:
+    #             address += self.billing_city
+    #     if self.billing_state:
+    #         if address:
+    #             address += ", " + self.billing_state
+    #         else:
+    #             address += self.billing_state
+    #     if self.billing_postcode:
+    #         if address:
+    #             address += ", " + self.billing_postcode
+    #         else:
+    #             address += self.billing_postcode
+    #     if self.billing_country:
+    #         if address:
+    #             address += ", " + self.get_billing_country_display()
+    #         else:
+    #             address += self.get_billing_country_display()
+    #     return address
 
     @property
     def created_on_arrow(self):
         return arrow.get(self.created_on).humanize()
 
-    @property
-    def contact_values(self):
-        contacts = list(self.contacts.values_list('id', flat=True))
-        return ','.join(str(contact) for contact in contacts)
+    # @property
+    # def contact_values(self):
+    #     contacts = list(self.contacts.values_list('id', flat=True))
+    #     return ','.join(str(contact) for contact in contacts)
 
 
 class Email(models.Model):
--- CRM/models_contacts.py
+++ CRM/models_contacts.py
@@ -2,24 +2,37 @@ import arrow
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from common.models import Address, User
+# from common.models import Address, User
+from django.conf import settings    #import user
+from company.models import Company  #import company
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Contact(models.Model):
+    # adding company details
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_contacts')
+
+    # adding user
+    assigned_to = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='contact_assigned_users')
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='contact_created_by',
+        on_delete=models.SET_NULL, null=True)
+
+
+
     first_name = models.CharField(_("First name"), max_length=255)
     last_name = models.CharField(_("Last name"), max_length=255)
     email = models.EmailField(unique=True)
     phone = PhoneNumberField(null=True, unique=True)
-    address = models.ForeignKey(
-        Address, related_name='adress_contacts',
-        on_delete=models.CASCADE, blank=True, null=True)
+    # address = models.ForeignKey(
+    #     Address, related_name='adress_contacts',
+    #     on_delete=models.CASCADE, blank=True, null=True)
+
+    address = models.CharField(_("Address"), max_length=500, blank=True, null=True)
+
     description = models.TextField(blank=True, null=True)
-    assigned_to = models.ManyToManyField(
-        User, related_name='contact_assigned_users')
-    created_by = models.ForeignKey(
-        User, related_name='contact_created_by',
-        on_delete=models.SET_NULL, null=True)
+    
     created_on = models.DateTimeField(_("Created on"), auto_now_add=True)
     is_active = models.BooleanField(default=False)
 
